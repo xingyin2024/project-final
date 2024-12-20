ef7cc823d538dfa1fdbb9ac0e150b7fc10c88e63
xingyin2024 <xingyin.thu@gmail.com>
2024-12-20 11:43:20 +0100

    add authentication and update app.post

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ dotenv.config();// Load environment variables from the .env file
 
 // Defining port and connecting to mongoose
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/traktamente";
+console.log("MongoDB Connection String:", mongoUrl);  
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
 
@@ -19,18 +20,23 @@ const userSchema = new mongoose.Schema({
     type: String,
     unique: true,
     required: true,
+    trim: true,
+    minlength:[2, "Username must be at least 2 characters long"]
   },
   password: {
     type: String,
-    required: true,
+    required: true,   
+    minlength:[6, "Username must be at least 6 characters long"]
   },
   firstName: {
     type: String,
     required: true,
+    trim: true,    
   },
   lastName: {
     type: String,
     required: true,
+    trim: true,
   },
   email: {
     type: String,
@@ -50,11 +56,26 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
+module.exports = User;
 
+//Authenticate user as middleware
+const authenticateUser = async (req, res, next) => {
+  // Authenticate accessToken existing
+  const accessToken = req.header("Authorization")
+  if (!accessToken) {
+    return res.status(401).json({ error: "Unauthorized: Missing access token" })
+  }
 
-
-
-
+  // find user with right accessToken
+  const user = await User.findOne({ accessToken })
+  if (user) {
+    console.info("User is found", user)
+    req.user = user
+    next()
+  } else {
+    return res.status(403).json({ error: "Forbidden: Invalid access token" })
+  }
+}
 
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
@@ -72,10 +93,16 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.get("/users", (req, res) => {
-  res.json({ message: "this is the users data api" })
+// get users, for test purpose, shall NOT have!
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
-
+ 
 app.post("/users", async (req, res) => {
   try {
     const { username, password, firstName, lastName, email, role } = req.body;
@@ -98,9 +125,22 @@ app.post("/users", async (req, res) => {
   }
 });
 
+app.get("/secrets", authenticateUser);
 app.get("/secrets", (req, res) => {
   res.json({ message: "this is a secret message" })
 });
+
+app.post("/sessions", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    // if user found and the password is correct
+    res.json({ id: user._id, accessToken: user.accessToken })
+  } else {
+    // if user not found
+    res.json({ notFound: true })
+  }
+})
+
 
 // App endpoints documentation
 app.get("/", (req, res) => {
