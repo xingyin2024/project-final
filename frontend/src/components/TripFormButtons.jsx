0861fbd51af0625483f84b1d3592ba32f4422028
xingyin2024 <xingyin.thu@gmail.com>
2025-01-21 16:38:37 +0100

    fix bug of defaultProps in TripFormButtons

--- frontend/src/components/TripFormButtons.jsx
+++ frontend/src/components/TripFormButtons.jsx
@@ -5,16 +5,14 @@ import '../styles/tripForm.css';
  * Renders two buttons: Save and Cancel.
  * The parent is responsible for all logic (onSave, onCancel, etc.).
  */
-const TripFormButtons = ({ onSave, onCancel, disabledSave }) => {
+const TripFormButtons = ({ onSave = null, onCancel, disabledSave = false }) => {
   return (
     <div className="trip-form-actions">
       <div className="trip-form-actions-row">
         {/* The Save button can be "type=submit" if we rely on the parent's <form onSubmit=...> */}
         <button
           type="submit"
-          className={`primary-btn ${
-            disabledSave ? 'primary-btn-disabled' : ''
-          }`}
+          className="primary-btn"
           disabled={disabledSave}
           onClick={onSave}
         >
@@ -33,11 +31,6 @@ TripFormButtons.propTypes = {
   onSave: PropTypes.func,
   onCancel: PropTypes.func.isRequired,
   disabledSave: PropTypes.bool,
-};
-
-TripFormButtons.defaultProps = {
-  onSave: null,
-  disabledSave: false,
 };
 
 export default TripFormButtons;
