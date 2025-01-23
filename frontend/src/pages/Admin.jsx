e65c4786484596667898737c1b3fe386b77e4770
xingyin2024 <xingyin.thu@gmail.com>
2025-01-23 19:57:12 +0100

    disable add user

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -65,12 +65,12 @@ const Admin = () => {
         {/* Team Member Header */}
         <div className="team-header-container">
           <h2 className="team-header">Team Member</h2>
-          <button
+          {/* <button
             className="text-btn"
             onClick={() => console.log('Add Team Member')}
           >
             Add Team Member
-          </button>
+          </button> */}
         </div>
 
         {/* Search Input */}
